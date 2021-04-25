99337ac61c902b89e890939c7465545158e40e79
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-04-25 22:05:59 +0300

    fix (start_consuming.py): add django initialization

--- start_consuming.py
+++ start_consuming.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bridge.settings')
+import django
+django.setup()
+
 from bridge.rabbitmq import broker
 from bridge.settings import networks
 
